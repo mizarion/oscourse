4a503cea03795125b4c7d36ec028e5a562b3112b
zsgluschenko <zsgluschenko@gmail.com>
2023-03-07 07:10:34 +0300

    Implement DW_FORM_block2 dwarf.c

--- kern/dwarf.c
+++ kern/dwarf.c
@@ -71,6 +71,17 @@ dwarf_read_abbrev_entry(const void *entry, unsigned form, void *buf, int bufsize
     case DW_FORM_block2: {
         /* Read block of 2-byte length followed by 0 to 65535 contiguous information bytes */
         // LAB 2: Your code here
+
+        // Взял реализацию из DW_FORM_block1
+        uint32_t length = get_unaligned(entry, Dwarf_Half);
+        entry += sizeof(Dwarf_Half);
+        struct Slice slice = {
+                .mem = entry,
+                .len = length,
+        };
+        if (buf) memcpy(buf, &slice, sizeof(struct Slice));
+        entry += length;
+        bytes = length + sizeof(Dwarf_Half);
     } break;
     case DW_FORM_block4: {
         uint32_t length = get_unaligned(entry, uint32_t);
